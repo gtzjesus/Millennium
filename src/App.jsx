6f3bc4fa696962c17077b456c2f07d20f555a15e
gtz.jesus <gtz.jesus@outlook.com>
2023-11-22 13:57:43 -0700

    black and white vid

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Landing from './pages/landing/Landing';
 import './styles/App.css';
+import Navigation from './ui/navigation/Navigation';
 
 function App() {
   return (
     <>
+      <Navigation />
       <Landing />
     </>
   );
---
+++ src/ui/navigation/Navigation.jsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+const StyledNav = styled.nav`
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  z-index: var(--z-top);
+  margin: var(--margin-small) 0 0 0;
+  // FONTS
+  font-size: var(--font-xsmall);
+  // ARRANGE CHILDREN
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Brand = styled.span`
+  // ARRANGE CHILDREN
+  display: flex;
+  align-items: center;
+  padding: 0 0 0 var(--padding-small);
+  gap: var(--gap-small);
+`;
+
+const Menu = styled.div`
+  padding: 0 var(--padding-small) 0 0;
+`;
+function Navigation() {
+  return (
+    <StyledNav>
+      <Brand>
+        <img src="/logos/worldhello.png" alt="globe" />
+        WorldHello
+      </Brand>
+      <Menu>Menu</Menu>
+    </StyledNav>
+  );
+}
+
+export default Navigation;
